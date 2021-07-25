43f7c873810f2e573f33ff3695e42bd737957f63
一腔诗意在哪 <zaze97@163.com>
2021-07-25 15:03:33 +0800

    插件上传

---
+++ Assets/Data/Input/InputReader.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using MyInputSystem;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+[CreateAssetMenu(fileName = "InputReader", menuName = "Game/Input Reader")]
+public class InputReader : ScriptableObject, MyInputSystem.InputControls.IGameActions
+{
+    public event UnityAction<Vector2> MoveSelectionEvent = delegate { };
+    private InputControls gameInput;
+    private void OnEnable()
+    {
+        if (gameInput == null)
+        {
+            gameInput = new InputControls();
+            gameInput.Game.SetCallbacks(this);
+        }
+
+        EnableGameplayInput();
+    }
+
+    private void OnDisable()
+    {
+        DisableAllInput();
+    }
+    public void EnableGameplayInput()
+    {
+        //当前开启别的结束，只存在一个操作系统
+        gameInput.Game.Enable();
+    }
+    public void DisableAllInput()
+    {
+        //结束全部
+        gameInput.Game.Disable();
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+            MoveSelectionEvent.Invoke(context.ReadValue<Vector2>());
+    }
+}
