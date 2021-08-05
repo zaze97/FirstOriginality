2f4c0a5b6241b8d8630ec98d7fdc109f03064625
zaze97 <83420172@qq.com>
2021-08-05 18:09:12 +0800

    111

--- Assets/Editor/test.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(menuName="MySubMenue/Create MyScriptableObject ")]
-public class test:ScriptableObject 
-{
-    public int someVariable;
-    public List<string> dic;
-}
